ff236d9ed9bab87e79c6223672fdebe33dba5642
fellpsgn <fellps.program06@gmail.com>
2024-02-04 10:46:53 -0300

    Ajustado nome de funções; variaveis initial e finished movidas para inicio da aplicação; Adicionado restart de tentativas e limpeza do input

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
-const specialNumber = Math.round(Math.random() * 10);
 let attempts = 0;
+let specialNumber = Math.round(Math.random() * 10);
+const initial = document.querySelector(".screen1");
+const finished = document.querySelector(".screen2");
+
 console.log(specialNumber);
 
-function handleClick(event) {
+function handleTryClick(event) {
     event.preventDefault();
     const inputNumber = document.querySelector("#inputNumber");
     attempts++;
 
     if (Number(inputNumber.value) === specialNumber) {
-        const initial = document.querySelector(".screen1");
-        const finished = document.querySelector(".screen2");
-
         const content = document.querySelector(".screen2 h2");
         content.innerText = `Acertou em ${attempts} tentativa(s)!!`;
 
         initial.classList.add("hide");
         finished.classList.remove("hide");
-
-
     }
 
+    inputNumber.value = "";
+
 }
 
-function handleChange() {
-    const initial = document.querySelector(".screen1");
-    const finished = document.querySelector(".screen2");
-
+function handleAgainClick() {
     initial.classList.remove("hide");
     finished.classList.add("hide");
+    attempts = 0;
+    specialNumber = Math.round(Math.random() * 10);
 }
+
+//Eventos 
+const btnTry = document.querySelector("#btnTry");
+const btnAgain = document.querySelector("#btnAgain");
+
+btnTry.addEventListener('click', handleTryClick);
+btnAgain.addEventListener('click', handleAgainClick);
+
 
